c24c21604b47cf41f0c53edfd279ce6b6a180ba6
tomohiro_0225 <express22212270@gmail.com>
2024-01-14 14:41:09 +0900

    planのタブ切り替え完成

--- assets/js/common.js
+++ assets/js/common.js
@@ -17,3 +17,21 @@ $(".faq__question--3").click(function() {
 $(".faq__question--4").click(function() {
     $(".faq__answer--4").slideToggle();
 });
+// planのタブ切り替え
+$(document).ready(function () {
+    // 初期状態では最初のタブをアクティブにする
+    $('.plan__tab:first').addClass('active');
+    $('.plan__menu:first').addClass('active');
+
+    // タブがクリックされたときの処理
+    $('.plan__tab').on('click', function () {
+        // すべてのタブとコンテンツから active クラスを削除
+        $('.plan__tab').removeClass('active');
+        $('.plan__menu').removeClass('active');
+
+        // クリックされたタブに active クラスを追加
+        var tabId = $(this).data('tab');
+        $('#' + tabId).addClass('active');
+        $(this).addClass('active');
+    });
+});
