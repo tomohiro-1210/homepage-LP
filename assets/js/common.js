7dcb89eb8f734e525a5f395d56d47b6e1d87ebb7
tomohiro_0225 <express22212270@gmail.com>
2024-01-14 09:52:27 +0900

    why you canとpackageの構築完了

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,19 @@
+// headerのハンバーガーメニュー
 $(".l-header__hamburger").click(function () {
     $(this).toggleClass('open');
     $(".sp-menu").toggleClass('open');
+});
+
+// lp-topのfaq
+$(".faq__question--1").click(function() {
+    $(".faq__answer--1").slideToggle();
+});
+$(".faq__question--2").click(function() {
+    $(".faq__answer--2").slideToggle();
+});
+$(".faq__question--3").click(function() {
+    $(".faq__answer--3").slideToggle();
+});
+$(".faq__question--4").click(function() {
+    $(".faq__answer--4").slideToggle();
 });
