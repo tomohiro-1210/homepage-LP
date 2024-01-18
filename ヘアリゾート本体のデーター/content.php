5571de03384afe52dee797be050af6e68b6859f4
tomohiro_0225 <express22212270@gmail.com>
2024-01-18 12:24:36 +0900

    LP確認完了 かずとさんのフォーム確認も無事OK！

--- ヘアリゾート本体のデーター/content.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$headers = <<<HEAD
-From :lounge0810@gmail.com 
-Return-Path: lounge0810@gmail.com 
-Content-Type: text/plain;charset=ISO-2022-JP 
-HEAD;
-$result = mail("lounge0810@gmail.com","テストのメールタイトル","テストのメール本文",$headers);
-var_dump($result);
