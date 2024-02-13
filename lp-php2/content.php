e55017f82916cef7733481d55400e1faa8218ecf
tomohiro_0225 <express22212270@gmail.com>
2024-02-13 15:15:56 +0900

    no message

--- lp-php2/content.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$headers = <<<HEAD
-From :express22212270@gmail.com 
-Return-Path: express22212270@gmail.com 
-Content-Type: text/plain;charset=ISO-2022-JP 
-HEAD;
-$result = mail("express22212270@gmail.com","テストのメールタイトル","テストのメール本文",$headers);
-var_dump($result);
